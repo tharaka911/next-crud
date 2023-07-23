d502b957e5621e446ecb449ddf4d2e6d45108928
tharaka911 <tharakahitibandara@gmail.com>
2023-07-23 21:48:53 +0530

    before pc clean

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,26 +1,89 @@
+// import Link from "next/link";
+// import RemoveBtn from "./RemoveBtn";
+// import { HiPencilAlt } from "react-icons/hi";
+
+// export default async function TopicsList() {
+//   const { topics } = await getTopics();
+
+//   return (
+//     <>
+//       {topics?.map((t) => (
+//         <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5" key={t._id} >
+//           <div>
+//             <h2 className="font-bold text-2xl">{t.title}</h2>
+//             <div>{t.description}</div>
+//           </div>
+
+//           <div className="flex gap-2 items-start">
+//             <RemoveBtn />
+//             <Link href={`/editTopic/${t._id}`}>
+//               <HiPencilAlt size={24} />
+//             </Link>
+//           </div>
+//         </div>
+//       ))}
+//     </>
+//   );
+// }
+
+// const getTopics = async () => {
+//   const res = await fetch("http://localhost:3000/api/topics", {
+//     caches: "no-store",
+//   });
+
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch topics");
+//   }
+
+//   const topics = await res.json();
+
+//   return topics;
+// };
+
+
 import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
-import{HiPencilAlt} from "react-icons/hi";
+import { HiPencilAlt } from "react-icons/hi";
 
-export default function TopicsList() {
-  return (
-    <>
-      <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5">
+const getTopics = async () => {
+  const res = await fetch("http://localhost:3000/api/topics", {
+    caches: "no-store",
+  });
 
-        <div>
-          <h2 className="font-bold text-2xl">Topic Title</h2>
-          <div>Topic Description</div>
-        </div>
+  if (!res.ok) {
+    throw new Error("Failed to fetch topics");
+  }
 
-        <div className="flex gap-2 items-start">
-          <RemoveBtn />
-          <Link href={'/editTopic/123'}>
-           <HiPencilAlt size={24} />
-          </Link>
+  const topics = await res.json();
 
-        </div>
+  return topics;
+};
 
-      </div>
-    </>
-  );
+export default async function TopicsList() {
+  try {
+    const { topics } = await getTopics();
+
+    return (
+      <>
+        {topics.map((t) => (
+          <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5" key={t._id}>
+            <div>
+              <h2 className="font-bold text-2xl">{t.title}</h2>
+              <div>{t.description}</div>
+            </div>
+
+            <div className="flex gap-2 items-start">
+              <RemoveBtn />
+              <Link href={`/editTopic/${t._id}`}>
+                <HiPencilAlt size={24} />
+              </Link>
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  } catch (error) {
+    return <div>Error: {error.message}</div>;
+  }
 }
+
